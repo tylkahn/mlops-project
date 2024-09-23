009049859f1681fd9bbb20d1bea91face38a2a92
tylkahn <tylkahn@gmail.com>
2024-09-23 14:05:34 -0700

    update kubernetes

--- src/flows/project_train_flow.py
+++ src/flows/project_train_flow.py
@@ -1,4 +1,4 @@
-from metaflow import FlowSpec, step, Parameter, kubernetes
+from metaflow import FlowSpec, step, Parameter, kubernetes, retry, timeout
 
 class TrainFlow(FlowSpec):
     path = Parameter('path', type=str, required=True)
@@ -14,9 +14,13 @@ class TrainFlow(FlowSpec):
         self.ntrees = [20,40,60,80,100]
         self.next(self.train_rf, foreach='ntrees')
 
-    @kubernetes(image='metaflow_env')
+    @timeout(hours=1)
+    @retry(times=0)
+    @kubernetes()
     @step
     def train_rf(self):
+        import os
+        os.system("pip install numpy scikit-learn mlflow")
         from sklearn.ensemble import RandomForestRegressor
         import mlflow
         mlflow.set_tracking_uri('https://mlops-19873747083.us-west2.run.app')
@@ -26,26 +30,33 @@ class TrainFlow(FlowSpec):
         with mlflow.start_run():
             self.model = RandomForestRegressor(n_estimators = self.input, max_features = 5, oob_score = True)
             self.model.fit(self.X, self.y)
+            self.run = mlflow.active_run().info.run_id
             r2 = self.model.oob_score_
-            mlflow.log_artifact(self.path)
+            mlflow.log_params({'n_estimators':self.input, 'max_features':5})
             mlflow.log_metric('r2', r2)
         mlflow.end_run()
         self.next(self.choose_model)
 
     @step
-    def choose_model(self, inputs):   
+    def choose_model(self, inputs):  
+        import mlflow
+        mlflow.set_tracking_uri('https://mlops-19873747083.us-west2.run.app')
+        mlflow.set_experiment('metaflow')
+
         def score(inp):
             r2 = inp.model.oob_score_
-            return inp.model, r2
+            return inp.model, r2, inp.run
 
         self.results = sorted(map(score, inputs), key=lambda x: -x[1])
         self.model = self.results[0][0]
+        runid = self.results[0][2]
+        mlflow.register_model(model_uri = f'runs:/{runid}', name = 'best_model')
         self.next(self.end)
 
     @step
     def end(self):
         print('Scores:')
-        print('\n'.join('%s %f' % res for res in self.results))
+        print('\n'.join('%s %f %s' % res for res in self.results))
         print('Model:', self.model)
 
 
